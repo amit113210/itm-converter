5181410fb097e2e80b550dd53d0c47b7f4f3875c
amit113210 <30905600+amit113210@users.noreply.github.com>
2025-07-28 00:27:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const proj4 = require('./proj4');
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-// הגדרת מערכת ITM לפי התקן הרשמי EPSG:2039
 proj4.defs(
   'EPSG:2039',
   "+proj=tmerc +lat_0=31.7343936111111 +lon_0=35.2045169444444 " +
     "+k=1.0000067 +x_0=219529.584 +y_0=626907.39 +ellps=GRS80 " +
-    "+towgs84=23.772,17.49,17.859,-0.3132,-1.85274,1.67299,-5.4262 " +
-    "+units=m +no_defs"
+    "+towgs84=0,0,0 +units=m +no_defs"
+
+
 );
 
 // פונקציה שממירה קו רוחב/אורך ל־ITM בעזרת Proj4js
